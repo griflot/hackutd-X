2fd7bfb599638b8d48cfe815c144a43c98531d0f
Griffin Perlot <gperlot9@gmail.com>
2023-11-05 10:35:40 -0600

    changed output of calculateApproval to tuple

--- Flask/calculate.py
+++ Flask/calculate.py
@@ -12,25 +12,24 @@ def maxMortgage(loss):
     return loss*0.45
 
 '''
-return codes:
-    1: likely to be approved
-    -1: error in given data
-    -2: credit score too low: advise to pay off credit cards on time
-    -3: LTV too high, might qualify with PMI: advise to either choose a larger down payment or purchase PMI (private mortgage insurance)
-    -4: LTV too high: advise to choose a larger down payment
-    -5: DTI too high: advise to choose a lower monthly mortgage rate
-    -6: DTI too high: advise to transfer high interest loans to low interest credit cards (but not have too many credit cards either)
-    -7: FEDTI too high: advise to choose a lower monthly mortgage rate
-    
+return tuples:
+    (1): likely to be approved
+    (-1): error in given data
+    (-2): credit score too low: advise to pay off credit cards on time
+    (-3, loweredAppraisedValue, raisedDownPayment): LTV too high, might qualify with PMI: advise to either choose a larger down payment or purchase PMI (private mortgage insurance)
+    (-4, loweredAppraisedValue, raisedDownPayment): LTV too high: advise to choose a larger down payment
+    (-5, lowerMortgage): DTI too high: advise to choose a lower monthly mortgage rate
+    (-6): DTI too high: advise to transfer high interest loans to low interest credit cards (but not have too many credit cards either)
+    (-7, lowerMortgage): FEDTI too high: advise to choose a lower monthly mortgage rate
 '''
 def calculateApproval(CreditScore, GrossMonthlyIncome, CreditCardPayment, StudentLoanPayment, CarPayment, MonthlyMortgagePayment, AppraisedValue, DownPayment, LoanAmount):
     if LoanAmount + DownPayment != AppraisedValue or MonthlyMortgagePayment > LoanAmount:
         print("invalid data given: value constraints not met")
-        return -1
+        return (-1, None, None)
     
     if CreditScore < 0 or GrossMonthlyIncome < 0 or CreditCardPayment < 0 or StudentLoanPayment < 0 or CarPayment < 0 or MonthlyMortgagePayment < 0 or AppraisedValue < 0 or DownPayment < 0 or LoanAmount < 0:
         print("invalid data given: data contains negative values")
-        return -1
+        return (-1, None, None)
 
     Credit_Pass = CreditScore > 640
     LTV_Pass = ((LoanAmount/AppraisedValue)*100) < 80
@@ -39,33 +38,37 @@ def calculateApproval(CreditScore, GrossMonthlyIncome, CreditCardPayment, Studen
     
     if ~Credit_Pass:
         print("Pay off debt etc etc")
-        return -2
+        return (-2, None, None)
     elif ~LTV_Pass:
         print("you got a LTV problem")
-        if LTVCalc(LoanAmount, AppraisedValue) < 95:
-            print("max acctpable price: " + str(maxAcceptableValue(DownPayment)))
-            print("min accptable down payment: " + str(minAcceptableDownpay(AppraisedValue)))
-            return -3
+        loweredAppraisedValue = maxAcceptableValue(DownPayment)
+        raisedDownPayment = minAcceptableDownpay(AppraisedValue)
+        print("max acctpable price: " + str(loweredAppraisedValue))
+        print("min accptable down payment: " + str(raisedDownPayment))
+        LTV = LTVCalc(LoanAmount, AppraisedValue)
+        if LTV >= 80 and LTV <= 95:
+            return (-3, loweredAppraisedValue, raisedDownPayment)
         else:
-            print("absolutly not")
-            return -4
+            return (-4, loweredAppraisedValue, raisedDownPayment)
     elif ~DTI_Pass:
         print("you got a dti problm")
         if DTICalc(CarPayment + CreditCardPayment + StudentLoanPayment + MonthlyMortgagePayment, GrossMonthlyIncome) < 43:
             if MonthlyMortgagePayment / (MonthlyMortgagePayment + CreditCardPayment + CarPayment + StudentLoanPayment) * 100 > 28:
                 print("Mortgage as percentage of total monthly debts is too high")
-                print("Better mortgage: " + str(maxMortgage(CarPayment + CreditCardPayment + StudentLoanPayment)))
-                return -5
+                lowerMortgage = maxMortgage(CarPayment + CreditCardPayment + StudentLoanPayment)
+                print("Better mortgage: " + str(lowerMortgage))
+                return (-5, lowerMortgage, None)
             else:
                 print("You must lower your DTI")
-                return -6
+                return (-6, None, None)
         else:
             print("You must lower your DTI")
-            return -6
+            return (-6, None, None)
     elif ~FEDTI_Pass:
         print("You must lower your FEDTI")
-        print("Better mortgage: "+ str(maxMortgage(CarPayment + CreditCardPayment + StudentLoanPayment)))
-        return -7
+        lowerMortgage = maxMortgage(CarPayment + CreditCardPayment + StudentLoanPayment)
+        print("Better mortgage: "+ str(lowerMortgage))
+        return (-7, lowerMortgage, None)
     else:
         print("you pass!")
-        return 1
+        return (1, None, None)
